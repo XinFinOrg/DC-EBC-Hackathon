1443fc1d764e023a299bf9f96e321652f61e6360
wieruszewski <piotrek.wieruszewski@gmail.com>
2023-10-25 16:34:03 +0200

    Update logic of a PerpetualDEX

--- contracts/PerpectualDex.sol
+++ contracts/PerpectualDex.sol
@@ -8,7 +8,7 @@ contract PerpetualDEX {
     IXRC20 public collateralToken;
     uint256 public lastFundingTimestamp;
     uint256 public fundingRate;
-    uint256 public indexPrice;
+    uint256 public currentPrice;
     uint256 public feeRate; // Trading fee rate, e.g., 0.005 for 0.5%
 
     enum Side { LONG, SHORT }
@@ -21,10 +21,10 @@ contract PerpetualDEX {
     }
 
     Position[] public positions;
-    mapping(address => uint256) public collateralBalances;
+    mapping(address => int256) public collateralBalances;
 
     event PositionOpened(address indexed trader, uint256 positionId, uint256 size, uint256 entryPrice, Side side);
-    event PositionClosed(address indexed trader, uint256 positionId, uint256 size, uint256 entryPrice, Side side, uint256 profit);
+    event PositionClosed(address indexed trader, uint256 positionId, uint256 size, uint256 entryPrice, Side side, int256 profit);
     event FundingPaid(uint256 fundingPayment);
 
     constructor(address _collateralToken) {
@@ -32,7 +32,7 @@ contract PerpetualDEX {
         collateralToken = IXRC20(_collateralToken);
         lastFundingTimestamp = block.timestamp;
         fundingRate = 0;
-        indexPrice = 1000; // Initial index price
+        currentPrice = 1000; // Initial index price
         feeRate = 500; // 0.5% fee
     }
 
@@ -42,9 +42,7 @@ contract PerpetualDEX {
         require(side == Side.LONG || side == Side.SHORT, "Invalid side");
 
         // Calculate collateral required
-        uint256 collateralRequired = (size * entryPrice) * indexPrice / 1e18;
-
-        require(collateralBalances[msg.sender] >= collateralRequired, "Insufficient collateral");
+        uint256 collateralRequired = (size * entryPrice)  / 1e18;
 
         positions.push(Position({
             trader: msg.sender,
@@ -53,7 +51,7 @@ contract PerpetualDEX {
             side: side
         }));
 
-        collateralBalances[msg.sender] -= collateralRequired;
+        collateralBalances[msg.sender] += int256(collateralRequired);
 
         uint256 positionId = positions.length - 1;
         emit PositionOpened(msg.sender, positionId, size, entryPrice, side);
@@ -69,13 +67,40 @@ contract PerpetualDEX {
         uint256 entryPrice = position.entryPrice;
         Side side = position.side;
 
-        uint256 pnl = calculateProfitAndLoss(positionId);
+        int256 pnl = calculateProfitAndLoss(positionId);
+        uint256 initialCost = getCostOfOpeningPosition(positionId);
+        uint256 fee = (uint256(initialCost) * feeRate) / 1e4;
+        int256 profit = pnl - int256(fee);
+        collateralBalances[admin] += int256(fee);
+        
+        int256 adminGets = -profit;
+        int256 traderGets = int256(initialCost) + profit;
+        if(traderGets > 0){
+            collateralToken.transfer(msg.sender, absoluteValue(traderGets));
+        }
+        
+        if(-profit > int256(initialCost)){
+            collateralBalances[msg.sender] -= int256(initialCost);
+            collateralBalances[admin] += profit + int256(initialCost);
+        } else
+        {
+            if(profit > 0)
+                collateralBalances[msg.sender] -= int256(initialCost);
+            else {
+                collateralBalances[msg.sender] -= traderGets;
+            }
 
-        uint256 fee = (pnl * feeRate) / 1e4;
-        uint256 profit = pnl - fee;
+            collateralBalances[admin] += adminGets;
+        }
 
-        collateralBalances[msg.sender] += (entryPrice * size * indexPrice) / 1e18;
-        collateralBalances[admin] += fee;
+        collateralBalances[msg.sender] += int256(currentPrice * size) / 1e18;
+        
+        if(collateralBalances[msg.sender] < 0) {
+            collateralBalances[admin] -= -collateralBalances[msg.sender];
+        } else {
+            collateralBalances[admin] += collateralBalances[msg.sender];
+        }
+        collateralBalances[msg.sender] = 0;
 
         emit PositionClosed(msg.sender, positionId, size, entryPrice, side, profit);
 
@@ -84,24 +109,29 @@ contract PerpetualDEX {
         positions.pop();
     }
 
-    function calculateProfitAndLoss(uint256 positionId) internal view returns (uint256) {
+    function calculateProfitAndLoss(uint256 positionId) internal view returns (int256) {
         Position storage position = positions[positionId];
         uint256 size = position.size;
         uint256 entryPrice = position.entryPrice;
         Side side = position.side;
 
-        uint256 currentPrice = indexPrice; // Replace with a real price source.
-
         if (side == Side.LONG) {
-            return (currentPrice * size - entryPrice * size) / 1e18;
+            return int256(currentPrice * size - entryPrice * size) * 1e18;
         } else {
-            return (entryPrice * size - currentPrice * size) / 1e18;
+            return int256(entryPrice * size - currentPrice * size) * 1e18;
         }
     }
 
-    function updateIndexPrice(uint256 newPrice) external {
+    function getCostOfOpeningPosition(uint256 positionId) internal view returns (uint256) {
+        Position storage position = positions[positionId];
+        uint256 size = position.size;
+        uint256 entryPrice = position.entryPrice;
+        return entryPrice * size * 1e18;
+    }
+
+    function updatecurrentPrice(uint256 newPrice) external {
         require(msg.sender == admin, "Only admin can update index price");
-        indexPrice = newPrice;
+        currentPrice = newPrice;
         uint256 timeElapsed = block.timestamp - lastFundingTimestamp;
         if (timeElapsed > 0) {
             updateFundingRate(timeElapsed);
@@ -112,16 +142,31 @@ contract PerpetualDEX {
     function updateFundingRate(uint256 timeElapsed) internal {
         // Implement a mechanism to calculate and update the funding rate based on timeElapsed.
         // This is a placeholder for educational purposes.
-        // Formula: fundingRate = (indexPrice - previousIndexPrice) / previousIndexPrice * 86400 / timeElapsed
-        fundingRate = (indexPrice * 1e18 - indexPrice * 1e18) * 86400 / timeElapsed / indexPrice;
+        // Formula: fundingRate = (currentPrice - previouscurrentPrice) / previouscurrentPrice * 86400 / timeElapsed
+        fundingRate = (currentPrice * 1e18 - currentPrice * 1e18) * 86400 / timeElapsed / currentPrice;
     }
 
     function payFunding() external {
         uint256 timeElapsed = block.timestamp - lastFundingTimestamp;
         require(timeElapsed > 0, "Not time for funding payment yet");
         uint256 fundingPayment = (positions.length * fundingRate * timeElapsed) / 86400;
-        collateralBalances[admin] += fundingPayment;
+        collateralBalances[admin] += int256(fundingPayment);
         emit FundingPaid(fundingPayment);
         lastFundingTimestamp = block.timestamp;
+    }
+
+    function transferTokens(address to, uint256 value) public {
+        require(collateralToken.transfer(to, value), "Token transfer failed");
+    }
+
+    function getTokenBalance(address owner) public view returns (uint256) {
+        return collateralToken.balanceOf(owner);
+    }
+
+    function absoluteValue(int256 x) public pure returns (uint256) {
+        if (x < 0) {
+            return uint256(-x);
+        }
+        return uint256(x); 
     }
 }
